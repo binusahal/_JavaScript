89813b366a1f449dfacbdb92bc3c3b4883000af2
binusahal <sahal7648@gmail.com>
2025-01-30 23:30:51 +0300

    Scope and Closure

--- ScopeAndClosure.js
+++ ScopeAndClosure.js
@@ -24,3 +24,85 @@ function TestFun(){
 } 
 
 console.log(TestFun())  // design
+
+// we make function with a return statement
+
+function add(num1, num2){
+    return num1+num2
+
+}
+console.log(5+5)
+
+//BLOCK SCOPE:  U CAN GET ONLY IN IF STATEMENT AND FOR LOOP .
+
+let answer ="it's True"
+
+if (1 === 1){
+    let answer ="it's False"
+}
+console.log(answer) // it's True
+
+// for loop 
+for (let i=0; i<5; i++){
+    console.log(i)
+} 
+// 0 ,1,2,3,4
+
+// if we add var we can call the variable outside the loop
+
+for (let i=0; i<5; i++){
+
+    var gereeting = " salam"
+}
+
+console.log(gereeting) // salam
+
+// Hoisting  
+
+working()
+
+function working(){
+    console.log("ITS WORKING")
+}
+// ITS WORKING
+
+// Closure 
+
+let myname ="sahal"
+
+function sayhi(){
+
+    function greetingg(){
+        console.log(myname)
+    }
+
+    greetingg()
+}
+sayhi() // sahal
+
+
+// Reference 
+
+
+function game(sport){
+  
+    let score = 0
+    
+    return function win(){
+      score++
+      return `Your ${sport} game score is ${score}`
+    }
+    
+  }
+  
+  // we make reference 
+  let football = game("football")
+  
+  console.log(football()) // Your football game score is 1
+  // we can add more 
+  console.log(football()) // Your football game score is 2
+  
+  let basketball = game("basketball")
+
+    console.log(basketball()) // Your basketball game score is 1
+    console.log(basketball()) // Your basketball game score is 2
